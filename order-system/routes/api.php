d81d12d898aa7e894718fc1f0e24c3f73bda51e9
DESKTOP-PPMD04S\Kr004 <Kr004682zero@icloud.com>
2024-05-13 15:34:17 +0900

    コメント追加

--- order-system/routes/api.php
+++ order-system/routes/api.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+//middlewareはこれで作れる : php artisan make:middleware [ミドルウェア名]
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
